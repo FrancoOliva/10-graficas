2668e2849f7c9ddc362d92ac863650a6c9d9c807
Franco Oliva <franco.oliva1991@gmail.com>
2021-09-01 13:42:09 -0300

    Menu graficas app hecho

---
+++ 10 - graficasApp/src/app/shared/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent  {
+
+  menu: MenuItem[] = [
+    { ruta: 'graficas/barras', texto: 'Barras'},
+    { ruta: 'graficas/barras-dobles', texto: 'Barras Dobles'},
+    { ruta: 'graficas/dona', texto: 'Dona'},
+    { ruta: 'graficas/dona-http', texto: 'Dona Http'},
+  ];
+
+}
