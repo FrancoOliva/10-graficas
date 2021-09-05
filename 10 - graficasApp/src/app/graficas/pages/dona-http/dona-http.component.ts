1f7ea5d259db2cf2ea17296902abd543b52e5145
Franco Oliva <franco.oliva1991@gmail.com>
2021-09-05 19:15:58 -0300

    grafica dona con peticiones http

--- 10 - graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
+++ 10 - graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
@@ -8,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor() { }
+  // Doughnut
+  public doughnutChartLabels: Label[] = [
+    //'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others'
+  ];
+  public doughnutChartData: MultiDataSet = [
+    //[350, 450, 100, 50]
+  ];
+
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#FA250A', '#E0420F', '#FA6100', '#F007AF', '#ACF75C'
+      ]
+    }
+  ];  
+
+  constructor( private graficasServices: GraficasService ) { }
 
   ngOnInit(): void {
+
+    this.graficasServices.getUsuariosRedesSociales().subscribe( data => {
+      // console.log(data);
+
+      const labels = Object.keys( data ); // obtenemos solo las llaves
+      const values = Object.values( data ); // obtenemos solo los valores
+
+      this.doughnutChartLabels = labels;
+      this.doughnutChartData.push( values );
+    });
   }
 
 }
